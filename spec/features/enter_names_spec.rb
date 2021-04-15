b740bfd2828474c99723d2c6ac434dda49068bca
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-15 16:33:09 +0100

    test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,15 +1,7 @@
 
 feature 'Enter names' do 
   scenario "submitting names" do
-    visit '/'
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-
-    save_and_open_page
-
-    click_button 'Submit'
-
-    
+    sign_in_and_play
 
     expect(page).to have_content 'Charlotte vs Mittens'
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,13 +1,8 @@
 feature 'Hit points' do
   scenario "player 2s hit points" do
-    visit '/'
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
+    sign_in_and_play
 
+    expect(page).to have_content 'Mittens: 60HP'  
     save_and_open_page
-
-    click_button 'Submit'
-
-    expect(page).to have_content 'Mittens: 60HP'
   end
 end
