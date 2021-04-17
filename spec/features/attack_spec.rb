696ea42f73cd747c036a6e7aad0c554290f5cb00
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-17 11:25:23 +0100

    extracted the #attack method into a Game class

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,10 +7,11 @@ feature 'attack' do
     expect(page).to have_content 'Charlotte punched Mittens'
   end
 
-  scenario 'reduce Player 2 HP by 10' do
+  scenario 'reduces Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
     click_link 'Ok'
+    
     expect(page).not_to have_content 'Mitten:60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,6 +3,5 @@ feature 'View hit points' do
     sign_in_and_play
 
     expect(page).to have_content 'Mittens: 60HP'  
-    save_and_open_page
   end
 end
