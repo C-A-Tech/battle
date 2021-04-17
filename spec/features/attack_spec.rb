e1ec5d2f0836088072cdc8a81a8aa4a05b2fe6cb
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-17 18:35:53 +0100

    added multiplayer functionality

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,12 +7,26 @@ feature 'attack' do
     expect(page).to have_content 'Charlotte punched Mittens'
   end
 
+  # As Player 1,
+  # So I can start to win a game of Battle,
+  # I want my attack to reduce Player 2's HP by 10
   scenario 'reduces Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    click_link 'Ok'
+    click_button 'OK'
     
     expect(page).not_to have_content 'Mitten:60HP'
     expect(page).to have_content 'Mittens: 50HP'
+  end
+
+  # As Player 1,
+  # So I can lose a game of Battle,
+  # I want Player 2 to attack me, and I want to get a confirmation
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
+    expect(page).to have_content 'Mittens punched Charlotte'
   end
 end
